74e54b2d94ba8903e65f6a9ed7ef5128d730cbcd
Nadia <nadia_a2121@yahoo.com>
2024-05-28 11:38:48 +0330

    commit

--- app.java
+++ app.java
@@ -1,5 +1,13 @@
 public class app {
     public static void main(String[] args) {
-        System.out.println("Hello nadia");
+        long age=100;
+        int height =150;
+        long birthyear=1991;
+        long max=Long.MAX_VALUE;
+        float f3=12.36f;
+        double f4=12.659876;
+        boolean b1=true;
+        char c1='a';
+        System.out.println(c1);
     }
 }
